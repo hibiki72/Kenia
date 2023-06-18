8644fda91d637ba875e18f5324125916912f2f3d
鈴木響 <h.suzuki@piton.jp>
2023-06-18 16:44:44 +0900

    雛形

--- next_project/pages/index.tsx
+++ next_project/pages/index.tsx
@@ -2,33 +2,43 @@ import Link from 'next/link';
 import Head from 'next/head';
 import Layout, {siteTitle} from '@/components/layout';
 import utilStyles from '@/styles/utils.module.css';
+import { getSortedPostsData } from "@/lib/posts"
+import type { PostData } from "@/lib/posts"
+import Date from '../components/date';
 
-export default function Home() {
+type Props = {
+  allPostsData: PostData[];
+}
+export const getStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    }
+  }
+}
+
+export default function Home({ allPostsData }: Props) {
 	return (
-		<>
-			{/* <Head>
-				<title>Create Next App</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+		<Layout home>
 
-			<section>
-				<h1>
-					Read <Link href="/posts/first-post">this page!</Link>
-				</h1>
-			</section> */}
+			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
 
-      <Layout home>
-			<Head>
-				<title>{siteTitle}</title>
-			</Head>
-			<section className={utilStyles.headingMd}>
-				<p>[Your Self Introduction]</p>
-				<p>
-					(This is a sample website - you’ll be building a site like this on{' '}
-					<a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-				</p>
+				<ul className={utilStyles.list}>
+					{allPostsData.map(({ id, date, title }) => (
+						/* <li className={utilStyles.listItem} key={id}>
+
+						</li> */
+						<li className={utilStyles.listItem} key={id}>
+							<Link href={`/posts/${id}`}>{title}</Link>
+							<br />
+							<small className={utilStyles.lightText}>
+								<Date dateString={date} />
+							</small>
+						</li>
+					))}
+				</ul>
 			</section>
 		</Layout>
-		</>
 	);
 }
---
+++ next_project/pages/posts/[id].tsx
@@ -0,0 +1,63 @@
+import { FC } from 'react';
+import Layout from '@/components/layout';
+import type { GetStaticProps } from 'next';
+import { getAllPostIds, getPostData } from '@/lib/posts';
+import Head from "next/head";
+import Date from "@/components/date";
+import utilStyles from "@/styles/utils.module.css";
+
+export const getStaticPaths = async () => {
+    const paths = getAllPostIds();
+    return {
+        paths,
+        fallback: false,
+    }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const postData = await getPostData(params?.id as string);
+    return {
+        props: {
+            postData,
+        }
+    }
+}
+
+type PostData = {
+    postData: {
+        id: string;
+        title: string;
+        data: string;
+        contentHtml: string;
+    }
+}
+
+// const Post: FC = () => {
+const Post: FC<PostData> = ({ postData }) => {
+	// return <Layout>...</Layout>;
+    return (
+        <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+
+			{/* {postData.title}
+			<br />
+			{postData.id}
+			<br />
+			<Date dateString={postData.date} />
+			<br />
+			<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} /> */}
+            
+			<article>
+				<h1 className={utilStyles.headingXl}>{postData.title}</h1>
+				<div className={utilStyles.lightText}>
+					<Date dateString={postData.date} />
+				</div>
+				<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+			</article>
+
+        </Layout>
+    )
+};
+export default Post;
